9ed7fc241ff3c2d8b4db731bca18483f12054159
ayushsalescode <ayush.singh@salescode.ai>
2025-07-17 20:06:04 +0530

    basic premium splash screen

--- alphaflow/lib/widgets/branded_splash_screen.dart
+++ alphaflow/lib/widgets/branded_splash_screen.dart
@@ -1,64 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class BrandedSplashScreen extends StatefulWidget {
-  const BrandedSplashScreen({super.key});
-
-  @override
-  State<BrandedSplashScreen> createState() => _BrandedSplashScreenState();
-}
-
-class _BrandedSplashScreenState extends State<BrandedSplashScreen> {
-  double _opacity = 0.0;
-
-  @override
-  void initState() {
-    super.initState();
-    // Fade in
-    Future.delayed(const Duration(milliseconds: 400), () {
-      if (mounted) setState(() => _opacity = 1.0);
-    });
-    // Fade out after 1.8s
-    Future.delayed(const Duration(milliseconds: 1800), () {
-      if (mounted) setState(() => _opacity = 0.0);
-    });
-  }
+class BrandedSplashScreen extends StatelessWidget {
+  const BrandedSplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Center(
-            child: Lottie.asset(
-              'assets/splash_animation.json',
-              width: 220,
-              height: 220,
-              fit: BoxFit.contain,
-            ),
-          ),
-          Positioned(
-            bottom: 80,
-            left: 0,
-            right: 0,
-            child: AnimatedOpacity(
-              opacity: _opacity,
-              duration: const Duration(milliseconds: 600),
-              child: Text(
-                'Alpha Flow',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.2,
-                ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Alpha Flow',
+              style: GoogleFonts.sora(
+                color: Colors.white,
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.2,
               ),
+              textAlign: TextAlign.center,
             ),
-          ),
-        ],
+            const SizedBox(height: 4),
+            Text(
+              'BY BLACKMERE',
+              style: GoogleFonts.sora(
+                color: Colors.white70,
+                fontSize: 11,
+                fontWeight: FontWeight.w400,
+                letterSpacing: 1.1,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
